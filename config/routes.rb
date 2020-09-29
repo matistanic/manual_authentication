d92c683f54225938f8de809564669f3a078231c9
Matías <matias.mullerg@mail.udp.cl>
2020-09-29 19:20:22 -0300

    primer commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :stories
+  resources :users, only: [:new, :create, :show]
   root 'stories#index'
+  resources :sessions, only: [:create, :destroy, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
